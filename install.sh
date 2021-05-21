c53f1b6b0e40338dfc779e333683e564a982610f
Sos1ska <72695378+Sos1ska@users.noreply.github.com>
2021-05-21 17:21:36 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,3 +13,4 @@ rm -rf ~/Spider-Breaking/Termux.zip
 cd ~/Spider-Breaking/ && chmod -R 777 Termux
 clear
 echo 'cd ~/Spider-Breaking/Termux && python spider-breaking.py or python ~/Spider-Breaking/Termux/spider-breaking'
+rm -rf ~/Spider-Breaking/install.sh

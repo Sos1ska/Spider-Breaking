f1b0d07ab04dc5b7837c65cdd25a0159dc558494
Sos1ska <72695378+Sos1ska@users.noreply.github.com>
2021-05-21 17:19:53 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+apt update && apt upgrade
+pkg install python -y
+pip install --upgrade pip
+pip install requests
+pip install colorama 
+pip install urllib3
+pkg install unzip -y
+pkg install git -y
+pkg upgrade -y
+git clone https://github.com/Sos1ska/Spider-Breaking
+unzip ~/Spider-Breaking/Termux.zip
+rm -rf ~/Spider-Breaking/Termux.zip
+cd ~/Spider-Breaking/ && chmod -R 777 Termux
+clear
+echo 'cd ~/Spider-Breaking/Termux && python spider-breaking.py or python ~/Spider-Breaking/Termux/spider-breaking'

518906f492d1175a56fc59580a7604d68bf9842d
Sos1ska <72695378+Sos1ska@users.noreply.github.com>
2021-05-29 10:45:15 +0300

    Create Spider-Breaking.py

---
+++ Termux/Spider-Breaking.py
@@ -0,0 +1,37 @@
+import datetime
+import json
+import time
+import os
+
+from files.main import start_main
+from files.test import read_test_log, start_test
+from files.RecordConfig.record import read_start_license, no_read_license
+from files.wrn.warn import keyboard, file_not_found_log, file_not_found_test
+from files.chech_inet import check_inet
+
+date_write = datetime.datetime.now()
+
+with open('files/config/config.json') as f:
+    root_data = json.load(f)
+root_usr = root_data.get("root_name_usr").get("root_input_usr")
+
+def cl():
+    os.system("clear")
+
+try:
+    try:
+        FWS = open('files/log/log.txt', 'a', encoding='utf-8')
+    except FileNotFoundError:
+        file_not_found_log()
+    FWS.write(f'{root_usr}:StartCode "Spider-Breaking" - ' + str(date_write) + '\n')
+    FWS.close()
+    cl()
+    check_inet()
+    time.sleep(3)
+    cl()
+    start_test()
+    read_test_log()
+    cl()
+    start_main()
+except KeyboardInterrupt:
+    keyboard()

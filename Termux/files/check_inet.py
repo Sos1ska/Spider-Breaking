6064dfdfa1d7f1cb7cc3a92ef665e836a7f72426
Sos1ska <72695378+Sos1ska@users.noreply.github.com>
2021-05-29 10:47:55 +0300

    Create check_inet.py

---
+++ Termux/files/check_inet.py
@@ -0,0 +1,20 @@
+import datetime, os
+
+from files.wrn.warn import i_e_url_requ
+
+try:
+    import requests
+except ImportError:
+    i_e_url_requ()
+
+def cl():
+    os.system("clear")
+
+def check_inet():
+    try:
+        response = requests.get("http://www.google.com")
+        print()
+        print("Интернет есть")
+    except requests.ConnectionError:
+        print()
+        print("Интернета нету")
